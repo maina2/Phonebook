9e6ab11922d7607aa6c0d3f47a818de6ce04c91f
queens kisivuli <queenskisivuli@gmail.com>
2023-06-15 22:05:17 +0300

    grouped user controler functions

--- src/controllers/GroupUsersController.js
+++ src/controllers/GroupUsersController.js
@@ -3,54 +3,54 @@ import config from "../db/config.js";
 
 // Get all Groups with Phonebook details
 export const getGroupsWithPhonebook = async (req, res) => {
-  console.log("running");
-  //   try {
-  let pool = await sql.connect(config.sql);
-  const result = await pool.request().query(`
+  try {
+    let pool = await sql.connect(config.sql);
+    const result = await pool.request().query(`
       SELECT g.ID AS GroupID, g.GroupName, p.ID AS PersonID, p.FullName, p.MobileNumber, p.WorkNumber, p.Email, p.HomeAddress
       FROM Groups g
-      LEFT JOIN Phonebook p ON g.ID = p.GroupID
+      LEFT JOIN PhonebookGroups pg ON g.ID = pg.GroupID
+      LEFT JOIN Phonebook p ON pg.PhonebookID = p.ID
     `);
 
-  const groups = {};
-  result.recordset.forEach((row) => {
-    const {
-      GroupID,
-      GroupName,
-      PersonID,
-      FullName,
-      MobileNumber,
-      WorkNumber,
-      Email,
-      HomeAddress,
-    } = row;
-    if (!groups[GroupID]) {
-      groups[GroupID] = {
+    const groups = {};
+    result.recordset.forEach((row) => {
+      const {
         GroupID,
         GroupName,
-        Phonebook: [],
-      };
-    }
-    groups[GroupID].Phonebook.push({
-      PersonID,
-      FullName,
-      MobileNumber,
-      WorkNumber,
-      Email,
-      HomeAddress,
+        PersonID,
+        FullName,
+        MobileNumber,
+        WorkNumber,
+        Email,
+        HomeAddress,
+      } = row;
+      if (!groups[GroupID]) {
+        groups[GroupID] = {
+          GroupID,
+          GroupName,
+          Phonebook: [],
+        };
+      }
+      groups[GroupID].Phonebook.push({
+        PersonID,
+        FullName,
+        MobileNumber,
+        WorkNumber,
+        Email,
+        HomeAddress,
+      });
     });
-  });
 
-  const response = Object.values(groups);
-  console.log("running");
-  res.status(200).json(response);
-  //   } catch (error) {
-  //     res
-  //       .status(500)
-  //       .json({ error: "An error occurred while retrieving Group details" });
-  //   } finally {
-  //     sql.close(); // Close the SQL connection
-  //   }
+    const response = Object.values(groups);
+
+    res.status(200).json(response);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while retrieving Group details" });
+  } finally {
+    sql.close();
+  }
 };
 
 // Get a single Group with Phonebook details
@@ -59,11 +59,12 @@ export const getGroupWithPhonebook = async (req, res) => {
     const { id } = req.params;
     let pool = await sql.connect(config.sql);
     const result = await pool.request().input("GroupID", sql.Int, id).query(`
-        SELECT g.ID AS GroupID, g.GroupName, p.ID AS PersonID, p.FullName, p.MobileNumber, p.WorkNumber, p.Email, p.HomeAddress
-        FROM Groups g
-        LEFT JOIN Phonebook p ON g.ID = p.GroupID
-        WHERE g.ID = @GroupID
-      `);
+      SELECT g.ID AS GroupID, g.GroupName, p.ID AS PersonID, p.FullName, p.MobileNumber, p.WorkNumber, p.Email, p.HomeAddress
+      FROM Groups g
+      LEFT JOIN PhonebookGroups pg ON g.ID = pg.GroupID
+      LEFT JOIN Phonebook p ON pg.PhonebookID = p.ID
+      WHERE g.ID = @GroupID
+    `);
 
     if (!result.recordset[0]) {
       res.status(404).json({ message: "Group not found" });
