03f020b6f5eac4b950c2fb45597a8210f0f5e91e
queens kisivuli <queenskisivuli@gmail.com>
2023-06-15 22:00:40 +0300

    server update

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
 import config from "./src/db/config.js";
-import PhoneRoutes from "./src/routes/PhoneRoutes.js";
+import PhoneBookRoutes from "./src/routes/PhoneRoutes.js";
+import auth from "./src/routes/auth.js";
+
+import userAuthorized from "./src/middleware/auth.js";
 
 const app = express();
-//
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//jwt middleware
+app.use(userAuthorized);
+
 app.use(bodyParser.json());
 
 // my-routes
-PhoneRoutes(app);
+app.use(express.json());
+PhoneBookRoutes(app);
+auth(app);
 
 app.get("/", (req, res) => {
   res.send("Hello😁 Welcome PhoneBook API!");
